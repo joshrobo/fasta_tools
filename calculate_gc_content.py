c6e38466159f393e7c3b6482d5cfc5cafee0f789
muku42 <muku42@users.noreply.github.com>
2015-10-05 18:18:33 -1000

    working on percentages, more to come

--- calculate_gc_content.py
+++ calculate_gc_content.py
@@ -8,6 +8,7 @@ a_count = 0
 c_count = 0
 g_count = 0
 t_count = 0
+base_total = 0
 
 with open(sys.argv[1], 'r') as fasta:
     for line in fasta:
@@ -18,9 +19,13 @@ with open(sys.argv[1], 'r') as fasta:
         c_count += line.count("C")
         g_count += line.count("G")
         t_count += line.count("T")
+    base_total += (a_count + c_count + g_count + t_count)
 
 print("Base counts for file %s:" % sys.argv[1])
 print("A: %d" % a_count)
 print("C: %d" % c_count)
 print("G: %d" % g_count)
 print("T: %d" % t_count)
+print("Base A percentage: ", round((float(a_count)/float(base_total)*100),2),"%")
+
+print("Total %d" % base_total)
