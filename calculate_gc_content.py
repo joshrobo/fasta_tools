768dda6ce70299e981816c0d741a582062b63c41
David L. Bennett <db42@muku42.local>
2015-10-07 10:41:01 -1000

    finished percentages

--- calculate_gc_content.py
+++ calculate_gc_content.py
@@ -21,11 +21,9 @@ with open(sys.argv[1], 'r') as fasta:
         t_count += line.count("T")
     base_total += (a_count + c_count + g_count + t_count)
 
-print("Base counts for file %s:" % sys.argv[1])
-print("A: %d" % a_count)
-print("C: %d" % c_count)
-print("G: %d" % g_count)
-print("T: %d" % t_count)
-print("Base A percentage: ", round((float(a_count)/float(base_total)*100),2),"%")
-
-print("Total %d" % base_total)
+print("\nBase counts for file %s:" % sys.argv[1])
+print("A: %d" % a_count), "which is", round((float(a_count)/float(base_total)*100),1),"% of the total"
+print("C: %d" % c_count), "which is", round((float(c_count)/float(base_total)*100),1),"% of the total"
+print("G: %d" % g_count), "which is", round((float(g_count)/float(base_total)*100),1),"% of the total"
+print("T: %d" % t_count), "which is", round((float(t_count)/float(base_total)*100),1),"% of the total"
+print''
